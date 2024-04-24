2c10df17061c518a2eb8e51a89f69e939f0c98c8
waheb-benzaid <waheb.benzaid@gmail.com>
2024-04-24 06:54:12 +0100

    add nums text files

--- cmd/main.go
+++ cmd/main.go
@@ -8,9 +8,18 @@ import (
 
 func main() {
 	if len(os.Args) > 1 {
-		fmt.Println(hello.Say(os.Args[1]))
+	fmt.Println(hello.Say(os.Args[1]))
 	} else {
 		fmt.Println("Hello World!")
 	}
-}
+
+	//declaration
+
+	a := 2
+	b := 2.1
+
+	fmt.Printf("a : %T %v \n",a,a)
+	fmt.Printf("b : %T %v \n",b,b)
+	}
+
 
